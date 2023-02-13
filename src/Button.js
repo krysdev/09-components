496999848a6f2e906e6465742e3242c5a25de235
KrysDev <webdevkrys@gmail.com>
2023-02-13 13:24:26 +0000

    x

--- src/Button.js
+++ src/Button.js
@@ -21,24 +21,25 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = libClassNames('flex items-center px-3 py-1.5 border', {
+  const classes = libClassNames('px-3 py-1.5 border', {
     'border-blue-500 bg-blue-500 text-white': primary === true,
     'border-gray-900 bg-gray-900 text-white': secondary,
     'border-green-500 bg-green-500 text-white': success,
     'border-yellow-400 bg-yellow-400 text-white': warning,
     'border-red-500 bg-red-500 text-white': danger,
     'rounded-full': rounded,
-    'bg-white': outline,
-    'text-blue-500': outline && primary,
-    'text-gray-900': outline && secondary,
-    'text-green-500': outline && success,
-    'text-yellow-400': outline && warning,
-    'text-red-500': outline && danger,
+    'bg-gray-200': outline,
+    'text-blue-600': outline && primary,
+    'text-gray-800': outline && secondary,
+    'text-green-600': outline && success,
+    'text-yellow-500': outline && warning,
+    'text-red-600': outline && danger,
   });
 
   // console.log(classes);
 
   return <button className={classes}>{children}</button>;
+  // return <button className='px-3 py-1.5 border bg-green-100 bg-green-500 text-white text-green-500'>{children}</button>;
 }
 
 Button.propTypes = {
