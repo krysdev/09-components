5b03b8adfe5c8f11b658486ed9df2ab31f1a9c27
KrysDev <webdevkrys@gmail.com>
2023-02-15 14:23:43 +0000

    x

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,44 @@
+import Button from '../components/Button';
+import { GoBell, GoDatabase, GoAlert, GoMail } from 'react-icons/go';
+
+function ButtonPage() {
+  const handleEvent = () => {
+    console.log('!! works !!');
+  };
+
+  return (
+    <div>
+      <div>
+        <Button success rounded outline onClick={handleEvent}>
+          <GoBell />
+          10000
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleEvent}>
+          <GoDatabase />
+          2001
+        </Button>
+      </div>
+      <div>
+        <Button warning className={'mb-5'}>
+          <GoAlert />
+          30000000
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoMail />
+          40000
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          50000
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
