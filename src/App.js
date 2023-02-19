c8c1ef003c49453cfd7bc74ffe6c1f16041a5076
KrysDev <webdevkrys@gmail.com>
2023-02-19 19:19:47 +0000

    x

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ function App() {
   const items = [
     {
       id: 'l2kj5',
-      label: 'Can I use React on a project?',
+      label: '1 Can I use React on a project?',
       content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        '1 You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
     },
     {
       id: 'lk2j35lkj',
-      label: 'Can I use Javascript on a project?',
+      label: '2 Can I use Javascript on a project?',
       content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        '2You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
     },
     {
       id: 'l1kj2i0g',
-      label: 'Can I use CSS on a project?',
+      label: '3 Can I use CSS on a project?',
       content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        '3 You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
     },
   ];
 
