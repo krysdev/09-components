46901b00dfca4b010ea3f280204c513b621c82f3
KrysDev <webdevkrys@gmail.com>
2023-02-21 10:45:41 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Dropdown from './components/Dropdown';
 
+const options = [
+  { label: 'Red', value: 'red' },
+  { label: 'Green', value: 'green' },
+  { label: 'Blue', value: 'blue' },
+];
+
+
 function App() {
-  return <Dropdown />;
+  return <Dropdown options={options} />;
 }
 
 export default App;
