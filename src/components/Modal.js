8e79eafaee26bf2bee24b4189ea0a5df22b15167
KrysDev <webdevkrys@gmail.com>
2023-02-28 19:52:43 +0000

    x

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,49 @@
-function Modal() {
-  
-  return (
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function Modal({ onClose, actionBar, children }) {
+  //
+  // to prevent scrolling when the Modal is open we add "overflow: hidden" to the BODY tag
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+    // and the clean up, when the modal is closed
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
+  // (first argument is the JSX, second argument is the refernece to the placeholder)
+  return ReactDOM.createPortal(
     <div>
-      modal
-    </div>
+
+      {/* this div is the whole gray background */}
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+
+      {/* this div is the white content */}
+      <div className="fixed inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+
+    </div>,
+    document.querySelector('.container-for-modal')
   );
 }
 
-export default Modal
+export default Modal;
+
+/*
+// to position the modal using the CSS 'fixed or absolute' and to reasure that it will never break
+// we have to use createPortal
+// and then this placeholder has to be added to the public/index.html file, right before the closing BODY tag:
+
+  <div class="container-for-modal"></div>
+ </body>
+</html>
+
+*/
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,12 +1,50 @@
-import Modal from "../components/Modal";
+import { useState } from 'react';
+import Button from '../components/Button';
+import Modal from '../components/Modal';
 
 function ModalPage() {
-  
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      Important message
+    </Modal>
+  );
+
   return (
     <div>
-      <Modal/>
+      <Button primary onClick={handleClick}>
+        Open modal
+      </Button>
+      
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, nobis perferendis! Nemo asperiores nesciunt minus architecto atque. Sed beatae maiores aliquam labore corrupti placeat tenetur itaque. Quas, facere. Doloribus, illum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, nobis perferendis! Nemo asperiores nesciunt minus architecto atque. Sed beatae maiores aliquam labore corrupti placeat tenetur itaque. Quas, facere. Doloribus, illum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, nobis perferendis! Nemo asperiores nesciunt minus architecto atque. Sed beatae maiores aliquam labore corrupti placeat tenetur itaque. Quas, facere. Doloribus, illum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde modi voluptate voluptates obcaecati laboriosam numquam mollitia non perspiciatis provident, sed, sit, aut animi iure totam qui excepturi consectetur? Est, molestias?
+      </p>
+
+      <Button primary onClick={handleClick}>
+        Open modal
+      </Button>
+
+      {showModal && modal}
+
     </div>
   );
 }
 
-export default ModalPage
+export default ModalPage;
