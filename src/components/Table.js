8c52c4d6c92c27097561df551b19fe7ebb222d97
KrysDev <webdevkrys@gmail.com>
2023-02-28 20:46:31 +0000

    x

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,28 @@
-function Table({children}) {
-  
+function Table({ data }) {
+  const renderedRows = data.map((el) => {
+    return (
+      <tr className="border-b" key={el.name}>
+        <td className="p-3">{el.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${el.colour}`}></div>
+        </td>
+        <td className="p-3">{el.score}</td>
+      </tr>
+    );
+  });
+
   return (
-    <div>
-      TABLE
-    </div>
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Friut</th>
+          <th>Colour</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
   );
 }
 
-export default Table
+export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,12 +1,18 @@
-import Table from "../components/Table";
+import Table from '../components/Table';
 
 function TablePage() {
-  
+  const data = [
+    { name: 'Orange', colour: 'bg-orange-500', score: 5 },
+    { name: 'Apple', colour: 'bg-red-500', score: 3 },
+    { name: 'Banana', colour: 'bg-yellow-500', score: 1 },
+    { name: 'Lime', colour: 'bg-green-500', score: 4 },
+  ];
+
   return (
     <div>
-      <Table/>
+      <Table data={data} />
     </div>
   );
 }
 
-export default TablePage
+export default TablePage;
