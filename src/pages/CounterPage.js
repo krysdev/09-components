d8ad682029d79711480378914a5bee35f71649a1
KrysDev <webdevkrys@gmail.com>
2023-03-08 21:53:38 +0000

    x

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import PillReusableComponent from '../components/PillReusableComponent';
 
+const reducer = (state, action) => {
+  //
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
   const handleClickPlus = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
   const handleClickMinus = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (e) => {
@@ -18,18 +26,18 @@ function CounterPage({ initialCount }) {
     // But when parseInt gets an empty string (''), so when you delete everything from the field, then it gives back 'NaN'
     // therefore NaN || 0 -> gives 0
     const value = parseInt(e.target.value) || 0;
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <PillReusableComponent className="m-3">
-      <h1>Count: {count}</h1>
+      <h1>Count: {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={handleClickPlus}>increase</Button>
         <Button onClick={handleClickMinus}>decrease</Button>
@@ -41,7 +49,7 @@ function CounterPage({ initialCount }) {
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
           // if valueToAdd is 0 then put an empty string instead
-          value={valueToAdd || ''}
+          value={state.valueToAdd || ''}
           onChange={handleChange}
         />
         <Button>Add this</Button>
